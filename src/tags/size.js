731f18bc021fe3e9e8d27356c5b98054a108ebae
xeroe <janlauw@gmx.de>
2021-07-28 01:42:30 +0200

    fix size tag

--- src/tags/size.js
+++ src/tags/size.js
@@ -1,16 +1,19 @@
 // https://github.com/vishnevskiy/bbcodejs/blob/master/src/coffee/tags.coffee
-import React from 'react';
-import Tag from '../tag';
+import React from "react";
+import Tag from "../tag";
 
 export default class SizeTag extends Tag {
-
   toHTML() {
-    const size = this.params.size;
+    const size = this.params.size / 100;
 
     if (isNaN(size)) {
       return this.getContent();
     }
-    return [`<span style="font-size:${size}px">`, this.getContent(), '</span>'];
+    return [
+      `<span style="font-size:${size}em;line-height: ${size}em;">`,
+      this.getContent(),
+      "</span>",
+    ];
   }
 
   toReact() {
@@ -21,8 +24,9 @@ export default class SizeTag extends Tag {
     }
 
     return (
-      <span style={{ fontSize: `${size}px` }}>{this.getComponents()}</span>
+      <span style={{ fontSize: `${size}em`, lineHeight: `${size}em` }}>
+        {this.getComponents()}
+      </span>
     );
   }
 }
-
